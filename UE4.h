1d080abfed13a71e0d4b0fc003917d4648aae93f
HyperionCSharp <94261284+HyperionCSharp@users.noreply.github.com>
2022-08-17 11:24:51 -0500

    Adding UE4 functions for better improvement

---
+++ UE4.h
@@ -0,0 +1,15 @@
+#include "UE/other.h"
+// Windows Files
+#include <Windows.h>
+#include <chrono>
+#include <vector>
+#include <string>
+#include <cstdint>
+
+//
+// HyperionCSharp: Feel free to add to this guys..
+//
+
+//
+// HyperionCSharp: Will add some unreal functions here soon.
+//
